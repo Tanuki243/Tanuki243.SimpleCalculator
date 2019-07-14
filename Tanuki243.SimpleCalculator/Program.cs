6645a4b69d247788a458c7448493119b62fa6b46
Tanuki243 <steffinhio@gmail.com>
2019-07-14 19:26:25 +0200

    Removals and Documation
    Removed unused Code

--- Tanuki243.SimpleCalculator/Program.cs
+++ Tanuki243.SimpleCalculator/Program.cs
@@ -8,26 +8,28 @@ namespace Tanuki243.SimpleCalculator
         {
             Rechenoperation ro = new Rechenoperation();
 
+            // Declare Variables
             string zahl1String;
             string operation;
             string zahl2String;
-
-            string val;
+            
             Console.Write("Enter integer: ");
-            do
-            {
-                zahl1String = Console.ReadLine();
-            } while ();
+            
+            // Read User Input
+            zahl1String = Console.ReadLine();
             operation = Console.ReadLine();
             zahl2String = Console.ReadLine();
 
+            // Convert Strings to Integer
             int zahl1 = Int32.Parse(zahl1String);
             int zahl2 = Int32.Parse(zahl2String);
 
+            // Call operation to calculate result
             int ergebnis = ro.Rechnen(zahl1, zahl2, operation); ;
 
             Console.WriteLine("Ergebnis " + ergebnis);
 
+            // Close Console
             Console.ReadLine();
         }
     }
